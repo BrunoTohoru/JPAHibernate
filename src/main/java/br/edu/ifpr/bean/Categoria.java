273f85af418297bf79c9381583d1d49493fb6674
Your Name <you@example.com>
2024-11-01 22:38:14 -0300

    01/11

---
+++ src/main/java/br/edu/ifpr/bean/Categoria.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.bean;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+/**
+ *
+ * @author Aluno
+ */
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    @Column(nullable = false)
+    private String nome;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
--- src/main/java/br/edu/ifpr/bean/Teste.java
+++ src/main/java/br/edu/ifpr/bean/Teste.java
@@ -3,11 +3,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.edu.ifpr.bean;
-import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 /**
  *
  * @author Aluno
  */
 public class Teste {
     
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysql_db");
+        EntityManager em = emf.createEntityManager();
+        
+        /**
+        *Categoria categoria = new Categoria();
+        *categoria.setNome("Moveis");
+        *
+        *em.getTransaction().begin();
+        *em.persist(categoria);
+        *em.getTransaction().commit();
+        *em.close();
+        *emf.close();
+        **/
+        
+        Ator ator = new Ator();
+        ator.setNome("Keanu Reeves");
+        ator.setCidade("New York");
+        ator.setIdade(52);
+        ator.setSalario(22.50);
+        ator.setSexo('M');
+        
+        em.getTransaction().begin();
+        em.persist(ator);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+        
+    }
 }
